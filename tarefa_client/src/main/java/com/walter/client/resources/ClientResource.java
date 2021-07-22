f32d36d1a7bd9445b8509fcc41b166e2e552ab36
waltersilveriojunior@hotmail.com <waltersilveriojr>
2021-07-22 17:32:21 -0300

    projeto completo

--- tarefa_client/src/main/java/com/walter/client/resources/ClientResource.java
+++ tarefa_client/src/main/java/com/walter/client/resources/ClientResource.java
@@ -1,17 +1,22 @@
 package com.walter.client.resources;
 
-import java.util.List;
+import java.net.URI;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.walter.client.dto.ClientDto;
 import com.walter.client.services.ClientService;
@@ -25,7 +30,7 @@ public class ClientResource {
 	
 	@GetMapping
 	public ResponseEntity<Page<ClientDto>> findAll(@RequestParam(value = "page", defaultValue = "0") Integer page,
-			@RequestParam(value = "linesPerPage", defaultValue = "6") Integer linesPerPage,
+			@RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
 			@RequestParam(value = "direction", defaultValue = "ASC") String direction,
 			@RequestParam(value = "orderBy", defaultValue = "name") String orderBy){
 		
@@ -41,4 +46,25 @@ public class ClientResource {
 		return ResponseEntity. ok().body(clientDto);
 	}
 	
+	@PostMapping
+	private ResponseEntity<ClientDto> insert(@RequestBody ClientDto client) {
+		client = clientService.insert(client);
+		URI uri = ServletUriComponentsBuilder
+				  .fromCurrentRequest().path("/{id}")
+				  .buildAndExpand(client.getId())
+				  .toUri();
+		return ResponseEntity.created(uri).body(client);
+	}
+	
+	@PutMapping(value = "/{id}")
+	private ResponseEntity<ClientDto> update(@PathVariable Long id, @RequestBody ClientDto client) {
+		client = clientService.update(id,client);
+		return ResponseEntity. ok().body(client);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	private ResponseEntity<Void> delete(@PathVariable Long id) {
+		clientService.delete(id);
+		return ResponseEntity.noContent().build();
+	}
 }
--- tarefa_client/src/main/java/com/walter/client/services/ClientService.java
+++ tarefa_client/src/main/java/com/walter/client/services/ClientService.java
@@ -2,7 +2,10 @@ package com.walter.client.services;
 
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -18,19 +21,61 @@ public class ClientService {
 
 	@Autowired
 	private ClientRepository clientRepository;
-	
+
 	@Transactional(readOnly = true)
-	public Page<ClientDto> findAllPaged(PageRequest pageRequest){
+	public Page<ClientDto> findAllPaged(PageRequest pageRequest) {
 		Page<Client> list = clientRepository.findAll(pageRequest);
 		return list.map(x -> new ClientDto(x));
 	}
-	
+
 	@Transactional(readOnly = true)
-	public ClientDto findById(Long id){
+	public ClientDto findById(Long id) {
 		Optional<Client> objClient = clientRepository.findById(id);
 		Client entity = objClient.orElseThrow(() -> new ClientNotFoundException("Client not found"));
 		return new ClientDto(entity);
 	}
-	
-	
+
+	@Transactional
+	public ClientDto insert(ClientDto client) {
+
+		Client entity = new Client();
+
+		entity.setName(client.getName());
+		entity.setCpf(client.getCpf());
+		entity.setIncome(client.getIncome());
+		entity.setBirthDate(client.getBirthDate());
+		entity.setChildren(client.getChildren());
+		entity = clientRepository.save(entity);
+
+		return new ClientDto(entity);
+	}
+
+	@Transactional
+	public ClientDto update(Long id, ClientDto client) {
+
+		try {
+			Client entity = clientRepository.getOne(id);
+
+			entity.setName(client.getName());
+			entity.setCpf(client.getCpf());
+			entity.setIncome(client.getIncome());
+			entity.setBirthDate(client.getBirthDate());
+			entity.setChildren(client.getChildren());
+
+			entity = clientRepository.save(entity);
+			return new ClientDto(entity);
+
+		} catch (EntityNotFoundException e) {
+			throw new ClientNotFoundException("Id not found " + id);
+		}
+	}
+
+	public void delete(Long id) {
+		try {
+			clientRepository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ClientNotFoundException("Id not found " + id);
+		}
+	}
+
 }
